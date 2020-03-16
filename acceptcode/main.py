b0cd060e71fff72c7bf4197b5953b7b5d65ec6a9
Skorono <58297559+Skorono@users.noreply.github.com>
2020-03-16 22:03:16 +0300

    Add files via upload

--- acceptcode/main.py
+++ acceptcode/main.py
@@ -1,3 +1,4 @@
 from acceptcode.EmailSend import SendYandexEmail 
 
-email = SendYandexEmail('astat.popov@yandex.ru','233833kasano','astat.popov@yandex.ru')
+email = SendYandexEmail('astat.popov@yandex.ru','233833kasano')
+
